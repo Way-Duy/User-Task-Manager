85a807a69e732da37f576b36bd9ee45aedf67dbe
J Hernandez <hernandezjg1234@gmail.com>
2020-10-07 15:30:41 -0500

    Added a general main menu where each button leads to a new page. Each page has a button to return to main menu. I created "Create New..." buttons on each page, but none of the buttons lead anywhere as of now.

--- src/com/company/JPanelManager.java
+++ src/com/company/JPanelManager.java
@@ -53,6 +53,7 @@ public class JPanelManager extends JFrame {
         loginPanel.add(buttonLogin, constraints);
 
         // set border for the panel
+        loginPanel.setPreferredSize(new Dimension(600, 600));
         loginPanel.setBorder(BorderFactory.createTitledBorder(
                 BorderFactory.createEtchedBorder(), "Login Panel"));
 
@@ -74,12 +75,348 @@ public class JPanelManager extends JFrame {
             }
         });
     }
-
+    
+    private JLabel labelUser = new JLabel("Hello User.");
+    private JButton buttonMembers = new JButton("View Member(s)");
+    private JButton buttonTeams = new JButton("View Team(s)");
+    private JButton buttonTasks = new JButton("View Task(s)");
+    private JButton buttonCategories = new JButton("View Categories");
     public void menu2() {
-        repaint();
+        //repaint();
         //TODO: create gui for menu2
-    }
+        // create a new panel with GridBagLayout manager
+        JPanel userPanel = new JPanel(new GridBagLayout());
 
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        userPanel.add(labelUser, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        userPanel.add(buttonMembers, constraints);
+        
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        userPanel.add(buttonTeams, constraints);
+        
+        constraints.gridx = 0;
+        constraints.gridy = 6;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        userPanel.add(buttonTasks, constraints);
+        
+        constraints.gridx = 0;
+        constraints.gridy = 8;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        userPanel.add(buttonCategories, constraints);
+
+        // set border for the panel
+        userPanel.setPreferredSize(new Dimension(600, 600));
+        userPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "User Panel"));
+
+        // add the panel to this frame
+        add(userPanel);
+
+        pack();
+        setLocationRelativeTo(null);
+        buttonMembers.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonMembers) // on pressing button
+                {
+                    userPanel.removeAll(); // removes all userPanel components
+                    menuMembers(); // moves onto the next menu
+                }
+            }
+        });
+        
+        buttonTeams.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonTeams) // on pressing button
+                {
+                    userPanel.removeAll(); // removes all userPanel components
+                    menuTeams(); // moves onto the next menu
+                }
+            }
+        });
+        
+        buttonTasks.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonTasks) // on pressing button
+                {
+                    userPanel.removeAll(); // removes all userPanel components
+                    menuTasks(); // moves onto the next menu
+                }
+            }
+        });
+        
+        buttonCategories.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonCategories) // on pressing button
+                {
+                    userPanel.removeAll(); // removes all userPanel components
+                    menuCategories(); // moves onto the next menu
+                }
+            }
+        });
+    }
+    
+    private JLabel labelMenuMembers = new JLabel("Current Members(s)");
+    private JButton buttonCreateMember = new JButton("Create New Member");
+    private JButton buttonReturn = new JButton("Main Menu");
+    public void menuMembers()
+    {
+        JPanel membersPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        membersPanel.add(labelMenuMembers, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        membersPanel.add(buttonCreateMember, constraints);
+        
+        constraints.gridx = 0;
+        constraints.gridy = 8;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        membersPanel.add(buttonReturn, constraints);
+
+        // set border for the panel
+        membersPanel.setPreferredSize(new Dimension(600, 600));
+        membersPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Members Panel"));
+
+        // add the panel to this frame
+        add(membersPanel);
+
+        pack();
+        setLocationRelativeTo(null);
+        buttonTasks.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonCreateTask) // on pressing button
+                {
+                    membersPanel.removeAll(); // removes all membersPanel components
+                    //menuCreateMember(); // moves onto the next menu
+                }
+            }
+        });
+        
+        buttonReturn.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonReturn) // on pressing button
+                {
+                    membersPanel.removeAll(); // removes all membersPanel components
+                    menu2(); // moves onto the next menu
+                }
+            }
+        });
+    }
+    
+    private JLabel labelMenuTeams = new JLabel("Current Team(s)");
+    private JButton buttonCreateTeam = new JButton("Create New Team");
+    
+    public void menuTeams()
+    {
+        JPanel teamsPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        teamsPanel.add(labelMenuTeams, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        teamsPanel.add(buttonCreateTeam, constraints);
+        
+        constraints.gridx = 0;
+        constraints.gridy = 8;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        teamsPanel.add(buttonReturn, constraints);
+
+        // set border for the panel
+        teamsPanel.setPreferredSize(new Dimension(600, 600));
+        teamsPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Teams Panel"));
+
+        // add the panel to this frame
+        add(teamsPanel);
+
+        pack();
+        setLocationRelativeTo(null);
+        buttonTasks.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonCreateTeam) // on pressing button
+                {
+                    teamsPanel.removeAll(); // removes all teamsPanel components
+                    //menuCreateTeam(); // moves onto the next menu
+                }
+            }
+        });
+        
+        buttonReturn.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonReturn) // on pressing button
+                {
+                    teamsPanel.removeAll(); // removes all teamsPanel components
+                    menu2(); // moves onto the next menu
+                }
+            }
+        });
+    }
+    
+    private JLabel labelMenuTasks = new JLabel("Current Task(s)");
+    private JButton buttonCreateTask = new JButton("Create New Task");
+    
+    public void menuTasks()
+    {
+        JPanel tasksPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        tasksPanel.add(labelMenuTasks, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        tasksPanel.add(buttonCreateTask, constraints);
+        
+        constraints.gridx = 0;
+        constraints.gridy = 8;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        tasksPanel.add(buttonReturn, constraints);
+
+        // set border for the panel
+        tasksPanel.setPreferredSize(new Dimension(600, 600));
+        tasksPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Tasks Panel"));
+
+        // add the panel to this frame
+        add(tasksPanel);
+
+        pack();
+        setLocationRelativeTo(null);
+        buttonTasks.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonCreateTask) // on pressing button
+                {
+                    tasksPanel.removeAll(); // removes all tasksPanel components
+                    //menuCreateTask(); // moves onto the next menu
+                }
+            }
+        });
+        
+        buttonReturn.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if( src == buttonReturn) // on pressing button
+                {
+                    tasksPanel.removeAll(); // removes all tasksPanel components
+                    menu2(); // moves onto the next menu
+                }
+            }
+        });
+    }
+    
+    public void menuCategories()
+    {
+        JPanel categoriesPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // add components to the panel        
+        constraints.gridx = 0;
+        constraints.gridy = 8;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        categoriesPanel.add(buttonReturn, constraints);
+
+        // set border for the panel
+        categoriesPanel.setPreferredSize(new Dimension(600, 600));
+        categoriesPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Categories Panel"));
+
+        // add the panel to this frame
+        add(categoriesPanel);
+
+        pack();
+        setLocationRelativeTo(null);
+        buttonReturn.addActionListener(new ActionListener() { // on pressing any button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object src = e.getSource();
+
+                if(src == buttonReturn)
+                {
+                    categoriesPanel.removeAll();
+                    menu2();
+                }
+            }
+        });
+    }
+    
     public static void main(String[] args) {
 
         try {
