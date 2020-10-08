4ca5967299f1914ca612a799d424d9ce4f055849
DominicGit1 <71572047+DominicGit1@users.noreply.github.com>
2020-10-08 18:02:42 -0500

    Update Task.java

--- src/com/company/Task.java
+++ src/com/company/Task.java
@@ -1,10 +1,11 @@
 package com.company;
-
-public class Task {
+public class Task 
+{
     String name, description, due_date, created_on, status;
     Member created_by, assigned_to;
     Task subtasks;
-    Task(String name, String description, String due_date, String created_on, String status, Member created_by, Member assigned_to, Task subtasks ){
+    Task(String name,String description,String due_date,String created_on,String status,Member created_by,Member assigned_to,Task subtasks)
+    {
         this.name = name;
         this.description = description;
         this.due_date = due_date;
@@ -14,7 +15,8 @@ public class Task {
         this. assigned_to = assigned_to;
         this.subtasks = subtasks;
     }
-    Task(String name, String description, String due_date, String created_on, String status, Member created_by, Member assigned_to ){
+    Task(String name,String description,String due_date,String created_on,String status,Member created_by,Member assigned_to)
+    {
         this.name = name;
         this.description = description;
         this.due_date = due_date;
@@ -25,15 +27,18 @@ public class Task {
         subtasks = null; //subtasks not necessary
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void assignColor(){ // not sure how to assign colors in terminal
+    public void assignColor()
+    { // not sure how to assign colors in terminal
 
     }
 
-    public String getInfo(){
+    public String getInfo()
+    {
         String info = "Name: " + name + "\nDescription: " + description + "\nDue Date: " + due_date +  "\nAssigned To: " + assigned_to.getUsername() +
                 "\nCreated On" + created_on +"\nCreated By: " + created_by.getUsername() + "\nStatus: " + status + "\nSubtasks: " +subtasks.getName();
         return info;
