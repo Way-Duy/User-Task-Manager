3ab3e043e323521c97c605b9460aeac73058a8db
DominicGit1 <71572047+DominicGit1@users.noreply.github.com>
2020-10-08 18:00:56 -0500

    Update Member.java

--- src/com/company/Member.java
+++ src/com/company/Member.java
@@ -1,16 +1,19 @@
 package com.company;
-
-public class Member {
+public class Member 
+{
     private String username, password; //immutable
-    Member(String username, String password){
+    Member(String username, String password)
+    {
         this.username = username;
         this.password = password;
     }
 
-    public String getUsername() {
+    public String getUsername() 
+    {
         return username;
     }
-    public boolean testPassword(String attempt){
+    public boolean testPassword(String attempt)
+    {
         if (attempt == password)
             return true;
         return false;
