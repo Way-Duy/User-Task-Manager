a4dd5eab10103c6a1c509c890949645956f14e43
DominicGit1 <71572047+DominicGit1@users.noreply.github.com>
2020-10-08 18:04:23 -0500

    Update Team.java

--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -1,29 +1,31 @@
-
 package com.company;
-
 import java.util.ArrayList;
 import java.util.List;
-
-public class Team {
+public class Team 
+{
     List<Member> team = new ArrayList<>();
     String name;
-    Team(String name, List<Member> team){
+    Team(String name, List<Member> team)
+    {
         this.name =name;
         this.team.addAll(team);
     }
 
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }
 
-    public List<Member> getTeam() {
+    public List<Member> getTeam() 
+    {
         return team;
     }
-    public void addMember(Member member){
+    public void addMember(Member member)
+    {
         team.add(member);
     }
-    public void deleteMember(Member member){
+    public void deleteMember(Member member)
+    {
         team.remove(member);
     }
-
 }
