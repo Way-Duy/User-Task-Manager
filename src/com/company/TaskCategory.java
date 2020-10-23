0b511115cf645be89e84c53634faa2008d78b14a
JSwinney24 <70666680+JSwinney24@users.noreply.github.com>
2020-10-23 16:34:52 -0500

    Update TaskCategory.java

--- src/com/company/TaskCategory.java
+++ src/com/company/TaskCategory.java
@@ -1,39 +1,69 @@
 package com.company;
 import java.util.ArrayList;
 import java.util.List;
-public class Team 
+public class TaskCategory 
 {
-    List<Member> team = new ArrayList<>();
-    String name;
-    String memberNames = "" ;
-    Team(String name)
+    String name, description, created_on;
+    String taskNames = "";
+    Member created_by;
+    List<Task> task_category_list = new ArrayList<>();
+    TaskCategory(String name, String description, String created_on, Member created_by)
     {
-        this.name =name;
+        this.name = name;
+        this.description = description;
+        this.created_on = created_on;
+        this.created_by = created_by;
     }
-    public String getName() 
+    public void addTask(Task task)
     {
-        return name;
-    }
-    public List<Member> getTeam() 
-    {
-        return team;
+        task_category_list.add(task);
     }
 
-    public String getMemberNames() {
-        return memberNames;
+    public String getTaskNames() {
+        return taskNames;
     }
 
-    public void addMemberString(String stringMember)
+    public void addTaskString(String taskName)
     {
         //team.add(member);
-        memberNames = memberNames + ",  " + stringMember;
+        taskNames = taskNames + ",  " + taskName;
     }
-    public void deleteMember(Member member)
+    public void deleteTask(Task task)
     {
-        team.remove(member);
+        task_category_list.remove(task);
     }
-    public void Edit(String name)
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCreated_on() {
+        return created_on;
+    }
+
+    public Member getCreated_by() {
+        return created_by;
+    }
+
+    public List<Task> getTask_category_list() {
+        return task_category_list;
+    }
+
+    public String getInfo()
     {
-        this.name =name;
+        String info = "Name: " + name + "\nDescription" + description +
+                "\nCreated On: " + created_on + "\nCreated By: " + created_by.getUsername();
+        return info;
+    }
+    public void Edit(String name, String description, String created_on, Member created_by)
+    {
+        this.name = name;
+        this.description = description;
+        this.created_on = created_on;
+        this.created_by = created_by;
     }
 }
