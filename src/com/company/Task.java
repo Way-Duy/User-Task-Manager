552584dd0b1f7e41320344dbb3a1ec98082ad354
JSwinney24 <70666680+JSwinney24@users.noreply.github.com>
2020-10-30 17:05:50 -0500

    Update Task.java

--- src/com/company/Task.java
+++ src/com/company/Task.java
@@ -77,4 +77,15 @@ public class Task
                 "\nCreated On" + created_on +"\nCreated By: " + created_by.getUsername() + "\nStatus: " + status + "\nSubtasks: " +subtasks.getName();
         return info;
     }
+   public void Edit(String name,String description,String due_date,String created_on,String status,Member created_by,Member assigned_to)
+    {
+        this.name = name;
+        this.description = description;
+        this.due_date = due_date;
+        this.created_on = created_on;
+        this.status = status;
+        this.created_by = created_by;
+        this. assigned_to = assigned_to;
+        subtasks = null; //subtasks not necessary
+    }
 }
