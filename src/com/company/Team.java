8cd30ec5370a212134c5a15f100662aea069903e
DominicGit1 <71572047+DominicGit1@users.noreply.github.com>
2020-12-01 20:37:40 -0600

    Update Team.java

--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -6,6 +6,19 @@ public class Team
     List<Member> team = new ArrayList<>();
     String name;
     String memberNames = "" ;
+    int taskPoints = 0;
+    public int getTaskPoints() // return team task points
+    {
+        return taskPoints;
+    }
+    public void completedTask() // add a point for task completed by team
+    {
+        taskPoints++;
+    }
+    public void clearTaskPoints() // clear the team points 
+    {
+        taskPoints = 0;
+    }
     Team(String name)
     {
         this.name =name;
